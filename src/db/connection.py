cbe0bbf392d581aeea018669801445ac0f94d568
Stryder <stryderschossberger11@gmail.com>
2025-11-14 10:12:36 -0600

    Included the testing data to be transfered to the the database when submitted to the UI and ability to pull it from the database to the ML model with an argument on location.

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,8 +1,11 @@
 # connection.py
 import sqlite3
 import streamlit as st
+import pandas as pd
 
-def save_to_database():
+
+# Sends training calls to DB from UI
+def send_training_data():
     """
     Saves detector and species data from session state to the Batlab.sql database.
     """
@@ -23,6 +26,12 @@ def save_to_database():
             cursor.executemany('INSERT INTO bats (Abbreviation, LatinName, CommonName) VALUES (?, ?, ?)', species_data)
             st.session_state.species = [] # Clear session state after saving
 
+        # Save Testing Data
+        if st.session_state.training_entries:
+            species_data = [(t['Species'], t['Location'], t['file']) for t in st.session_state.species]
+            cursor.executemany('INSERT INTO bats (Species, Location, File) VALUES (?, ?, ?)', species_data)
+            st.session_state.species = [] # Clear session state after saving
+
         conn.commit()
         st.success("✅ Data saved to database.")
 
@@ -33,3 +42,32 @@ def save_to_database():
     finally:
         if conn:
             conn.close()
+
+# Pulls training calls from DB to run in ML model
+def get_call_library_data(self, location=None):
+    conn = None  # Initialize connection to None
+
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect('batlab.SQL')
+
+        # Define the SQL query
+        query = "SELECT file, bat, location FROM call_library"
+        if location:
+                query += f" WHERE location = '{location}'"
+
+        # Execute the query and load the results into a pandas DataFrame
+        call_library_df = pd.read_sql_query(query, conn)
+
+        # Display the first few rows of the DataFrame
+        # display(call_library_df.head())
+
+    except FileNotFoundError:
+        print(f"Error: The database file was not found at {db_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Close the database connection
+        if 'conn' in locals() and conn:
+            conn.close()
+
